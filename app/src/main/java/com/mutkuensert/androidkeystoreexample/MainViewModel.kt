f77eb52886c85c1de4939340b0e2d800fc643874
M. Utku Ensert <mutkuensert@hotmail.com>
2024-06-25 15:14:08 +0300

    Save public key into shared preferences

--- app/src/main/java/com/mutkuensert/androidkeystoreexample/MainViewModel.kt
+++ app/src/main/java/com/mutkuensert/androidkeystoreexample/MainViewModel.kt
@@ -1,30 +1,45 @@
 package com.mutkuensert.androidkeystoreexample
 
+import android.app.Application
+import android.content.Context
+import androidx.core.content.edit
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
 import com.mutkuensert.androidkeystoreexample.keystorehelper.BiometricAuthHelper
 import com.mutkuensert.androidkeystoreexample.keystorehelper.KeyStoreHelper
-import com.mutkuensert.androidkeystoreexample.keystorehelper.SignedData
 import com.mutkuensert.androidkeystoreexample.keystorehelper.encodeBase64
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class MainViewModel : ViewModel() {
+private const val PrefsKeyPair = "keyPairPreferences"
+private const val KeyPublicKey = "publicKeyPrefsKey"
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val _uiModel = MutableStateFlow(MainUiModel.initial())
     val uiModel = _uiModel.asStateFlow()
 
     private val alias = "alias"
     private val keyStoreHelper = KeyStoreHelper(alias = alias, requireBiometricAuth = true)
 
+    private val preferences =
+        application.applicationContext.getSharedPreferences(PrefsKeyPair, Context.MODE_PRIVATE)
+
     fun init() {
         _uiModel.update {
-            it.copy(alias = alias)
+            it.copy(alias = alias, publicKey = preferences.getString(KeyPublicKey, "")!!)
         }
     }
 
     fun createKeyPair() {
         val keyPair = keyStoreHelper.generateHardwareBackedKeyPair()
+
+        if (keyPair != null) {
+            preferences.edit {
+                putString(KeyPublicKey, keyStoreHelper.getPublicKeyBase64Encoded(keyPair))
+            }
+        }
+
         _uiModel.update {
             if (keyPair != null) {
                 it.copy(
@@ -41,6 +56,8 @@ class MainViewModel : ViewModel() {
         val isDeleted = keyStoreHelper.deleteKeyStoreEntry()
         _uiModel.update {
             if (isDeleted) {
+                preferences.edit { remove(KeyPublicKey) }
+
                 it.copy(alias = "", publicKey = "")
             } else {
                 it
@@ -49,13 +66,10 @@ class MainViewModel : ViewModel() {
     }
 
     fun signData(activity: FragmentActivity) {
-        var signedData: SignedData?
         BiometricAuthHelper.authenticate(activity, onAuthenticationSucceeded = {
-            signedData = keyStoreHelper.signData(uiModel.value.data)
+            val signedData = keyStoreHelper.signData(uiModel.value.data) ?: return@authenticate
+            val signature: String = signedData.value.encodeBase64()
 
-            if (signedData == null) return@authenticate
-
-            val signature: String = signedData!!.value.encodeBase64()
             _uiModel.update {
                 it.copy(signature = signature)
             }
